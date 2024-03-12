8a00028ec29b3c08c40a6196e36072a78442e13b
Eli <eliunited@gmail.com>
2024-03-12 00:19:27 -0700

    changing back to adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-vercel';
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -9,13 +9,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			images: {
-				sizes: [640, 828, 1200, 1920],
-				formats: ['image/avif', 'image/webp'],
-				minimumCacheTTL: 300
-			}
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
