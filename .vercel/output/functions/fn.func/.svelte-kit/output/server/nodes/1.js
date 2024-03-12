758defa6f2db8db1ed748a520fe0ff86eda095d9
Eli Benton Cohen <eliunited@gmail.com>
2024-03-12 02:30:18 -0500

    Delete .vercel directory

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.D1PRRfKw.js","_app/immutable/chunks/scheduler.DUa3pFyD.js","_app/immutable/chunks/index.DKBitXsh.js","_app/immutable/chunks/entry.B69S1xRR.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.DbKZLWcf.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.B69S1xRR.js";export{t as start};
