f4449aed95048cbfb78dba70aa25334b4d014960
Eli <eliunited@gmail.com>
2024-07-26 13:55:43 -0400

    bio update

--- src/components/aboutComponent.svelte
+++ src/components/aboutComponent.svelte
@@ -2,18 +2,24 @@
 	// import { fade } from 'svelte/transition';
 </script>
 
-<div
-	class="mb-2 px-2 text-lg *:my-4 sm:mb-12 sm:*:text-2xl lg:px-8 lg:*:text-3xl">
+<div class="mb-2 px-2 text-lg *:my-4 sm:mb-12 sm:*:text-2xl lg:px-8">
 	<p>I am a freelance audio producer and reporter.</p>
 	<p>
-		I have made just about every kind of podcast: daily news, narrative series,
-		longform investigations, and talk shows. My reporting has taken me to
-		Kazakhstan, Myanmar, India, and all across the United States.
+		Over the past decade, I have made just about every kind of podcast
+		imaginable: daily news, narrative series, long-form investigations, even
+		live talk shows.
 	</p>
 	<p>
-		Some of my longstanding interests include American political realignment,
-		techno-utopianism, and Southern history. I also take photographs, some of
-		which you can view <a class="underline" href="https://photos.eli.fm">here</a
+		I've followed stories all across the United States and around the world —
+		interviewing an alleged Russian spy in Kazakhstan, following vigilantes
+		through the Myanmar hills, and tracking down state corruption in rural
+		India.
+	</p>
+	<p>
+		I've embedded in crypto communes, and interviewed Nobel laureates. The
+		stranger the story, the more I want to cover it. I also take photographs,
+		some of which you can view <a class="underline" href="https://photos.eli.fm"
+			>here</a
 		>.
 	</p>
 	<p>
