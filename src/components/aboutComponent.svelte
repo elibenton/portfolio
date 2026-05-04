a9a95740599898bc4c74708fd17c0f1ed31fcb50
Eli Benton Cohen <elicohen@wifi-10-44-111-146.wifi.berkeley.edu>
2026-05-04 13:37:49 -0700

    Pulitzer Finalist Update

--- src/components/aboutComponent.svelte
+++ src/components/aboutComponent.svelte
@@ -6,27 +6,21 @@
 	class="text-md mb-2 px-2 leading-[1.3] *:my-4 sm:mb-12 sm:text-2xl sm:leading-8 lg:px-8"
 >
 	<p>
-		I am a freelance journalist and graduate student at the Berkeley School of
+		I am an award-winning freelance journalist and graduate student at the Berkeley School of
 		Journalism.
 	</p>
 	<p>
-		Currently, I'm the host of <a
-			class="underline"
-			href="https://radiopacific.org/">Radio Pacific</a
-		>, a new show about California from KALW. I've produced audio storytelling
+	    Previously, I helped produce the New York Times podcast, <a class="underline" href="https://www.nytimes.com/2025/06/02/podcasts/trans-gender-care-protocol.html">The Protocol</a>, a finalist for the 2026 Pulitzer Prize in Audio. I've produced audio storytelling
 		of every kind: daily news, narrative series, long-form investigations, and
 		live talk shows.
 	</p>
 	<p>
-		My stories have taken me all across the United States and around the world —
-		interviewing an alleged Russian spy in Kazakhstan, following vigilantes
-		through the Myanmar hills, and tracking down state corruption in rural
-		India.
+		My stories have taken me all across the United States and around the world — speaking with unlawfully held immigrants in ICE detention, following vigilantes through the Myanmar hills, and meeting an alleged Russian spy in Kazakhstan.
 	</p>
 	<p>
 		I've embedded in crypto communes, visited the lab where more than 100
-		psychedelics were created, and interviewed Nobel laureates. The stranger the
-		story, the more I want to cover it. I also take photographs, some of which
+		psychedelics were first synthesized, and interviewed Nobel laureates. The more complicated the
+		story, the more I want to tell it. I also take photographs, some of which
 		you can view <a class="underline" href="https://photos.eli.fm">here</a>.
 	</p>
 	<p>
